e03e39c4a1904cc897a14512b4f9b6f2e49175a6
David-Vanuxin <106643037+David-Vanuxin@users.noreply.github.com>
2023-01-08 03:36:37 +0300

    Security problems solved

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
 	host: '127.0.0.1',
-	user: 'David',
-	password: '1234',
-	database: 'TodoApp'
+	user: /*mysql user username*/,
+	password: /*your mysql password*/,
+	database: 'TodoApp' // if you already created database using create-db.sql 
 })
 
 const create = (key, value) => {
